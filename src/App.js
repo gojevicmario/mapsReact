795249039e39267a688df9e5b0f8ff6c5bd84f08
gojevic.mario <gojevic.mario@gmail.com>
2019-12-25 16:26:36 +0100

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TableComponent from './TableComponent/TableComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TableComponent />
+    </div>
+  );
+}
+
+export default App;
